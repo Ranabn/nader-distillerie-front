8b2d0c24a76fa6f777bf6c3602b0cc57c18e6d88
Maxime Palmino <maximepalmino.pro@gmail.com>
2025-02-28 15:05:13 +0100

    Responsive

--- app/components/our-story/ResponsiveJourneyTimeline.tsx
+++ app/components/our-story/ResponsiveJourneyTimeline.tsx
@@ -7,31 +7,33 @@ export const ResponsiveJourneyTimeline = ({timeline}) => {
 
     return (
 
-        <Flex
+        <Box
             mt={40}
-            gap={[10, 20, 20]}
             alignItems={'center'} alignContent={'center'}
             flexDirection={["column", "row"]} justifyContent={'center'}>
             <Box textAlign={'center'} fontSize={["24px"]} fontFamily="EB Garamond" fontWeight="bold">
                 A journey of growth, skill, and unwavering dedication
             </Box>
             {timeline.map((item, index) => (
-                <Flex key={index} gap={10} justifyContent={'center'} alignItems={'center'} alignContent={'center'}>
-                    <Flex flexDirection="column" justifyContent={'center'} alignItems={'center'}
-                          alignContent={'center'}>
+                <Box key={index} justifyContent={'center'} alignItems={'center'} alignContent={'center'} my={10}
+                >
+                    <Box flexDirection="column" justifyContent={'center'} alignItems={'center'}
+                          alignContent={'center'}
+                    >
                         <Image src={item.imageUrl} alt={`drawing-${index}`} width={"100%"} height={"335px"}
                                objectFit="cover"/>
-                        <Flex flexDirection="column">
+                        <Flex flexDirection="column" 
+                        >
                             <Text fontSize={"20px"} mt={4}>{item.year}</Text>
                             <Text fontSize="28px" fontFamily="EB Garamond"
-                                  fontWeight="bold" width={'335px'}>{item.title}</Text>
-                            <Text mt={4} fontSize={"16px"} width={'335px'}>{item.description}</Text>
+                                  fontWeight="bold" >{item.title}</Text>
+                            <Text mt={4} fontSize={"16px"} >{item.description}</Text>
                         </Flex>
-                    </Flex>
-                </Flex>
+                    </Box>
+                </Box>
             ))}
 
-        </Flex>
+        </Box>
 
 
     );
